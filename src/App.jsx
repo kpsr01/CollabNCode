2109ad2fced95cc34d6dfed0f4c07ddb15e9d696
Konduru Pranav Suhas Reddy <pranavsuhasreddykonduru@gmail.com>
2024-09-29 15:20:46 +0530

    added vercel analytics

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import {BrowserRouter,Routes, Route} from 'react-router-dom'
 import Home from './pages/Home/Home'
 import Editorpage from './pages/Editor/Editorpage'
+import { Analytics } from "@vercel/analytics/react"
 
 function CollabNCode() {
   return (
